55abefa1019d83f097a0fe62867f4d8f287d1f49
FairyHome_QinLove <08172022612@qq.com>
2022-06-12 20:41:10 +0800

    Test02GitIDEACommit_second

--- src/main/java/tom/com/Test001.java
+++ src/main/java/tom/com/Test001.java
@@ -4,5 +4,6 @@ package tom.com;
 public class Test001 {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git01");
     }
 }
