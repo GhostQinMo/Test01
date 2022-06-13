91ea28f33add34647e62a4671d072d283d1b7286
FairyHome_QinLove <08172022612@qq.com>
2022-06-13 19:13:28 +0800

    Test02GitIDEACommit_FairyHome_fix First commit

--- src/main/java/tom/com/Test001.java
+++ src/main/java/tom/com/Test001.java
@@ -7,5 +7,6 @@ public class Test001 {
         System.out.println("hello git01");
         System.out.println("hello git02");
         System.out.println("fdsfsafa");
+        System.out.println("FairyHome_fix first Commit from IDEA  ");
     }
 }
